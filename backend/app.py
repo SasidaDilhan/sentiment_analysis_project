ce3de7d411e3346c351cac7a317715973dfb875b
sasida dilhan <it21158254@my.sliit.lk>
2025-10-13 18:09:12 +0530

    frontend changes done

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,8 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # allow React frontend to fetch data
 
 reviews = ['Good product', 'Bad Product', 'I like it']
 positive = 2
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/data")
+      .then((res) => res.json())
+      .then((data) => setData(data))
+      .catch((err) => console.error("Error:", err));
+  }, []);
+
+  if (!data)
+    return <p className="text-center mt-10 text-gray-600">Loading...</p>;
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center p-6">
+      <h1 className="text-3xl font-bold text-blue-700 mb-4">Product Reviews</h1>
+
+      <div className="bg-white shadow-lg rounded-lg p-6 w-full max-w-md">
+        <h2 className="text-xl font-semibold mb-2">Reviews:</h2>
+        <ul className="list-disc list-inside mb-4">
+          {data.reviews.map((review, index) => (
+            <li key={index} className="text-gray-700">
+              {review}
+            </li>
+          ))}
+        </ul>
+
+        <div className="flex justify-between">
+          <span className="text-green-600 font-semibold">
+            Positive: {data.positive}
+          </span>
+          <span className="text-red-600 font-semibold">
+            Negative: {data.negative}
+          </span>
+        </div>
+        <div className=" flex">
+          <form>
+            <textarea name="text"></textarea>
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
